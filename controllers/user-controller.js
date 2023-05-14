ac890b0fecf047bdd9957b8fb58963715e0b6679
victor <vjbsh@hotmail.com>
2023-05-14 14:15:52 -0600

    added logic to user to check if email already registered

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,25 +19,30 @@ function findAllUsers(req, res){
     });
 }
 
-function createUser(req, res){
+async function createUser(req, res) {
     console.log("Creating a user...");
+    const oldUser = await User.findOne(req.body.email);
+    if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+    }
+
     let user = new User({
         id: req.body.id,
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-
     });
-    user
-    .save()
-    .then((data) => {
-        return res.status(200).send({
-            error: false,
-            message: "Success",
-            data: data,
-            code: 10,
-        });
-    }).catch((error) => {
+
+    await user
+        .save()
+        .then((data) => {
+            return res.status(200).send({
+                error: false,
+                message: "Success",
+                data: data,
+                code: 10,
+            });
+        }).catch((error) => {
         return res.status(500).send({
             error: true,
             message: "Server error",
@@ -98,8 +103,8 @@ function findUserByIdAndUpdate(req, res){
 }
 
 function login(req, res){
-    const { name, password } = req.body
-    User.findOne({name, password }).then((data) => {
+    const { email, password } = req.body
+    User.findOne({email, password }).then((data) => {
         if(!data)
             res
                 .status(404).send({
